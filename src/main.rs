169d0979d1588591d98d176657191f2407cc3ea8
AbdulbasitSaid <mrspeeed518@gmail.com>
2026-08-10 06:05:46 +0100

    feat: added health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().body("Server is healthy")
-}
+use techsihir_newsletter::run;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
